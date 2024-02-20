ec0a2c488ed9d1826616d3fb264311aaaa20eb61
UsonTong <UsonTong@gmail.com>
2024-02-20 14:11:08 +0800

    Initial commit

---
+++ src/main/java/com/github/usontong/shortcutinventorylimit/ShortcutInventoryLimit.java
@@ -0,0 +1,9 @@
+package com.github.usontong.shortcutinventorylimit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShortcutInventoryLimit extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
